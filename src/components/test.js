1ab6ae95c0abf2260b597fddbebcaccfba365d2a
Solly <solobarine@gmail.com>
2022-10-27 20:38:43 +0100

    Refactor using hooks

---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+
+  const handleEvent = (e) => {
+  }
+
+  return (
+    <p></p> 
+  );
+}
+
+export default test
