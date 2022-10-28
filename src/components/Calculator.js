545390b2419cf6f194176f62a93406b02bc0d31b
Solly <solobarine@gmail.com>
2022-10-28 08:00:43 +0100

    Change operator / to ÷

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,7 +4,7 @@ import calculate from '../logic/calculate'; // eslint-disable-line
 const Calculator = () => {
   const [state, setState] = useState({ total: null, operation: null, next: null });
 
-  const figures = [{ val: 'AC' }, { val: '+/-' }, { val: '%' }, { val: '/' }, { val: 7 }, { val: 8 }, { val: 9 }, { val: 'x' }, { val: 4 }, { val: 5 }, { val: 6 }, { val: '-' }, { val: 1 }, { val: 2 }, { val: 3 }, { val: '+' }, { val: 0 }, { val: '.' }, { val: '=' }];
+  const figures = [{ val: 'AC' }, { val: '+/-' }, { val: '%' }, { val: '÷' }, { val: 7 }, { val: 8 }, { val: 9 }, { val: 'x' }, { val: 4 }, { val: 5 }, { val: 6 }, { val: '-' }, { val: 1 }, { val: 2 }, { val: 3 }, { val: '+' }, { val: 0 }, { val: '.' }, { val: '=' }];
   const id = 'item';
 
   const handleEvent = (e) => {
