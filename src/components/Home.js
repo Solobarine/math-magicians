fc87ef1cce1d252e43ee7ebbe836870a319ba737
Solomon <solobarine@gmail.com>
2023-04-18 01:14:59 +0100

    Add responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ const Home = () => {  //eslint-disable-line
 
   return (
     <div className="home">
-      <h2 className="title">Welcome to our page!</h2>
+      <h3 className="title">Welcome to our page!</h3>
       <p id="firstText">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus aut culpa magnam a quos ducimus repudiandae expedita doloribus, hic corrupti voluptate voluptatum nihil dolor architecto fugiat, amet consectetur dolorum ipsum totam obcaecati quibusdam maxime iste adipisci at? Libero, nesciunt dicta.</p>
       <p className="second">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ab hic ex rem, velit culpa minus fugiat accusantium magni blanditiis voluptatum officia accusamus porro autem reiciendis tenetur aperiam voluptatibus mollitia dicta consectetur quod. Odio eligendi aliquam dolor reiciendis vitae rem quisquam ullam minima! Mollitia dolor possimus vero consectetur saepe excepturi!</p>
     </div>
